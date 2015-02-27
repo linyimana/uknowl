334cfa38afd92346a3e13abf3ebe5a24a03f2f6e
linyiman <linyimana@gmail.com>
2015-02-27 16:40:00 +0800

    修改段积分模块，当用户为登录状态，浏览自己的词条，不记积分

--- extensions/Integral/Integral.body.php
+++ extensions/Integral/Integral.body.php
@@ -147,7 +147,9 @@ class Integral {
 			);
     		$author = self::object_array($author);
     		$uid = $author['rev_user'];
-
+    		if($author['rev_user'] == $uid){
+				return false;
+			}
     		//根据页面ID 查找页面内容
     		$content = $dbw->selectRow(
 				'text',
